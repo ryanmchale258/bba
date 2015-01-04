f21076ac315578a156b3391bcc91d1cdf2865567
ryanmchale258 <ryan.mchale258@gmail.com>
2015-01-04 15:59:07 -0500

    Nav fighting

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,16 +1,21 @@
 <header>
-	<section>
-		<nav id="desktop-nav" class="top-bar" data-topbar role="navigation">
-			<section class="top-bar-section">
-				<?php echo $navmenu; ?>
+	<section class="row small-collapse">
+		<div id="logoarea" class="small-10 small-centered medium-4 medium-uncentered large-3 columns">
+			<img src="<?php echo base_url(); ?>img/logo.jpg" alt="">
+		</div>
+		
+		<div id="navcontainer" class="hide-for-small medium-8 large-9 columns">
+			<div class="navinner">
+				<nav id="desktop-nav" class="top-bar" data-topbar role="navigation">
+				<section class="top-bar-section">
+					<?php echo $navmenu; ?>
+				</section>
+			</nav>
+			</div>
+		</div>
 
-				<ul class="right">
-					<li><a href="mission">Mission</a></li>
-					<li><a href="values">Values</a></li>
-				</ul>
-			</section>
-		</nav>
 	</section>
+
 </header>
 
 <div class="content-wrap">
