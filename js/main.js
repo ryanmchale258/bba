e91d5f037db1f014008b72c401f62a0862708e2d
ryanmchale258 <ryan.mchale258@gmail.com>
2015-01-04 17:23:24 -0500

    Slick buttons

--- js/main.js
+++ js/main.js
@@ -3,17 +3,23 @@
 	var theHeader = document.querySelector('header'),
 		theLogo = document.querySelector('#logoarea img'),
 		theFooter = document.querySelector('footer'),
-		theContent = document.querySelector('.content-wrap');
+		theContent = document.querySelector('.content-wrap'),
+
+	var logoHeight = theLogo.offsetHeight,
+		theHeader.style.height = logoHeight;
 
 	var	topPad = theHeader.offsetHeight,
 		logoHeight = theLogo.offsetHeight,
 		bottomPad = theFooter.offsetHeight;
 
-		theHeader.style.height = logoHeight,
+		theHeader.style.height = logoHeight;
 		theContent.style.paddingTop = topPad + "px";
 		theContent.style.paddingBottom = bottomPad + "px";
 
 	window.addEventListener('resize', function(){
+		var logoHeight = theLogo.offsetHeight,
+			theHeader.style.height = logoHeight;
+
 		var	topPad = theHeader.offsetHeight,
 			logoHeight = theLogo.offsetHeight,
 			bottomPad = theFooter.offsetHeight;
