261d235e72342d60e82a3b5edcfb97d86fd78b59
ryanmchale258 <ryan.mchale258@gmail.com>
2015-01-09 23:54:25 -0500

    Slider changes
    Still in progress

--- application/views/slider.php
+++ application/views/slider.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div id="sliderwrapper" class="row">
 	<div id="homeSlider" class="fade small-12 columns">
 		<div><h2>Residential Clinical Care</h2><img src="<?php echo base_url(); ?>img/slide1.gif"></div>
 		<div><h2>Administrative Support</h2><img src="<?php echo base_url(); ?>img/slide2.gif"></div>
