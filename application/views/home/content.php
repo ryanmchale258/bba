55f032f24106d2d2e423a718b680251b9f0e466f
ChristopherRobins <c_robins3@tss-hr312-dm24.local>
2015-02-23 13:43:03 -0500

    content

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -1,11 +1,13 @@
 <div class="row">
 	<div id="pagecontent" class="small-12 medium-8 columns">
 		<p>
-			Barker Blagrave &amp; Associates Dietetics Professional Corporation (BB&amp;A) is a team of qualified health care professionals who specialize in providing dietitian services to Long Term Care Homes. Currently, the Directors and Associates consult to 23 Long Term Care Homes in Central-West, Central-South and South-West Ontario.
+			Barker Blagrave &amp; Associates Dietetics Professional Corporation (BB&amp;A) is a team of qualified health care professionals who specialize in providing dietitian services to Long Term Care Homes. Currently, the Directors and Associates consult to several Long Term Care Homes across central and South-Western Ontario.
 		</p>
 
 		<p>
-			Our unique expertise and long term care (LTC) experience ensure the provision of high quality comprehensive dietetic services including resident clinical nutrition care,
-		administrative support, quality management and audits, dietetic resource development and staff education. We are committed to excellence in dietetic practice and believe that Registered Dietitians are an essential component of the health care team.
+			Our unique expertise and Long Term Care (LTC) experience ensure the provision of high quality comprehensive dietetic services including resident clinical nutrition care, administrative support, quality management and audits, dietetic resource development and staff education. We are committed to excellence in dietetic practice and believe that Registered Dietitians are key members of the health care team.
+		</p>
+		<p>
+			We provide dietetic services on both a long-term contract basis, for resident nutrition care in LTC Homes, and on a short term contract basis, for dietary services reviews/regulation compliance, nutrient analysis of residents’ menus, menu development, program audit and evaluation, and staff education sessions.
 		</p>
 	</div>
