2fe4e9a2405ba974286c2c4f2525e48d198b2366
ChristopherRobins <c_robins3@tss-hr312-dm24.local>
2015-02-23 14:50:30 -0500

    content and db

---
+++ application/views/about/services.php
@@ -0,0 +1,65 @@
+<div class="about row">
+	<section class="services" data-equalizer>
+		<h1>What we do</h1>
+		<p>
+			All Directors and Associates of BB&amp;A are Registered Dietitians and members of both the College of Dietitians of Ontario (the regulatory body for dietitians in Ontario) and the professional association, Dietitians of Canada. We are very familiar with the Ministry of Health and LTC Regulations for nutrition and hydration care and for dietary services, and with Accreditation processes through Accreditation Canada or CARF.  All of our Dietitians are also well versed in the latest electronic resident charting systems such as Point Click Care and Med e-Care.
+		</p>
+		<p>
+			The team of Registered Dietitians at Barker Blagrave&amp; Associates Dietetics Professional Corporation provides comprehensive Resident Clinical Nutrition Services, delivered with compassion and integrity in a manner that optimizes quality of care.
+		</p>
+		<p>
+			Our team also provides Administrative Support to Homes as needed, Quality Management tools and expertise and can provide Dietetic Resources and / or Teaching for staff in-service and ongoing education as well as Information Brochures for residents and families.
+		</p>
+		<h2>Services:</h2>
+		<article class="small-12 medium-6 columns" data-equalizer-watch>
+			<h3>LTC Home on-site Dietitian Services:</h3>
+			<ul>
+				<li>Nutrition assessment and monitoring</li>
+				<li>Nutrition care planning</li>
+				<li>Clinical nutrition interventions including, but not limited to:
+					<ul>
+						<li>Diabetes management</li>
+						<li>Dysphagia management</li>
+						<li>Nutrition in wound care management</li>
+						<li>Enteral feeding</li>
+						<li>Palliative care</li>
+					</ul>
+				</li>
+			</ul>
+		</article>
+		<article class="small-12 medium-6 columns" data-equalizer-watch>
+			<h3>Administrative Support:</h3>
+			<ul>
+				<li>Menu development, review and approval</li>
+				<li>Nutrient analysis of menus</li>
+				<li>Staff education and training</li>
+				<li>Policy development</li>
+				<li>Participation in accreditation processes</li>
+				<li>Dietary services reviews/regulation compliance</li>
+			</ul>
+		</article>
+		<article class="small-12 medium-6 columns" data-equalizer-watch>
+			<h3>Quality Management:</h3>
+			<ul>
+				<li>Audit tools and methodology</li>
+				<li>Interpretation of Ministry Regulations and Inspection Protocols</li>
+				<li>Program audit and evaluation</li>
+				<li>QM policies/procedures</li>
+				<li>Nutrition care services audits</li>
+				<li>Peer reviews</li>
+			</ul>
+		</article>
+		<article class="small-12 medium-6 columns" data-equalizer-watch>
+			<h3>Long Term Care Dietetic Resources/Teaching:</h3>
+			<ul>
+				<li>Professional workshops and presentations</li>
+				<li>Documentation tools</li>
+				<li>Qualified teachers for food service worker training program</li>
+				<li>Clinical and administrative policies/procedures</li>
+				<li>Algorithms/protocols for the care team</li>
+				<li>RAI-MDS tools and resources</li>
+				<li>Information brochures for residents/families</li>
+			</ul>
+		</article>
+	</section>
+</section>
--- application/views/template/scripts.php
+++ application/views/template/scripts.php
@@ -1,6 +1,7 @@
 	<script src="<?php echo base_url(); ?>js/vendor/jquery.js"></script>
     <script src="<?php echo base_url(); ?>js/foundation.min.js"></script>
     <script src="<?php echo base_url(); ?>js/foundation/foundation.offcanvas.js"></script>
+    <script src="<?php echo base_url(); ?>js/foundation/foundation.equalizer.js"></script>
     <script src="<?php echo base_url(); ?>slick/slick.min.js"></script>
 
     <script src="<?php echo base_url(); ?>js/main.js"></script>
