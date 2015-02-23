9f507db54f68169c3c4b514233d03a91caddb3fc
ryanmchale258 <r_mchale2@tss-hr312-dm23.local>
2015-02-23 14:49:44 -0500

    Menu

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,7 +5,7 @@
 		<img id="logo" src="<?php echo base_url(); ?>img/logo.png" alt="Barker Blagrave &amp; Associates Logo">
 	</div>
 
-		<div id="navcontainer" class="medium-8 large-8 columns">
+		<div id="navcontainer" class="small-2 medium-8 large-8 columns">
 			<div class="navinner">
 				<nav id="desktop-nav" class="top-bar hide-for-small" data-topbar role="navigation">
 					<section class="top-bar-section">
@@ -16,7 +16,7 @@
 				</nav>
 				<div class="navinner hide-for-medium-up">
 					<div id="navbutton">
-						<a id="ocbutton" href="#mobile-nav" role="button" aria-controls="mobile-nav" aria-expanded="false" class="right-off-canvas-toggle">Menu</a>
+						<a id="ocbutton" href="#mobile-nav" role="button" aria-controls="mobile-nav" aria-expanded="false" class="right-off-canvas-toggle menu-icon"><span></span></a>
 					</div>
 				</div>
 			</div>
@@ -30,7 +30,7 @@
 		<main class="off-canvas-wrap" data-offcanvas>
 			<div id="content" class="inner-wrap">
 
-	<aside id="mobile-nav" class="right-off-canvas-menu hide-for-large">
+	<aside id="mobile-nav" class="right-off-canvas-menu hide-for-large-up">
 		<ul class="off-canvas-list">
 			<?php echo $mobmenu; ?>
 		</ul>
