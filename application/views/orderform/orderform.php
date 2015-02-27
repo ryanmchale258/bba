fc90cf5e8dcd8b9efc09eb4b3b9193e5a5236ad9
ChristopherRobins <c_robins3@tss-hr312-dm24.local>
2015-02-27 14:00:42 -0500

    fixed stuff

--- application/views/orderform/orderform.php
+++ application/views/orderform/orderform.php
@@ -1,15 +1,17 @@
 <div class="bodycontent">
 	<h1>BB&amp;A Resource Order Form</h1>
 	<form class="order">
-		<label>1. Quality Care Audits, 1st Edition, September 2014</label>
+		<label class="product">1. Quality Care Audits, 1st Edition, September 2014</label>
 		<p>25 audits in the areas of Residents’ Dining, Food Production and Nutrition Care to support the Home’s Continuous Quality Improvement program</p>
 		CD <input type="number" name="quantity" min="0"> x $75.00
 		<br><br>
-		<label>2. Diabetes Update P&amp;P</label>
+		
+		<label class="product">2. Diabetes Update P&amp;P</label>
 		<p>Updated Policies &amp; Procedures Based on the 2013 Clinical Practice Guidelines</p>
 		CD <input type="number" name="quantity" min="0"> x $25.00 &nbsp;&nbsp;&nbsp;&nbsp;Email <input type="number" name="quantity" min="0"> x $25.00
 		<br><br>
-		<label>3. Education Essentials, 1st Edition</label>
+		
+		<label class="product">3. Education Essentials, 1st Edition</label>
 		<p>PP Presentations &amp; Participant Quizzes for Ensuring Quality Nutrition &amp; Hydration in LTC</p>
 		<div class="small-12 medium-6 columns">
 			<label><input type="checkbox" name="presentations" value="constipation">Constipation</label>
@@ -34,6 +36,33 @@
 			<label><input type="checkbox" name="presentations" value="foodsafety">Food Safety &amp; Sanitation</label>
 			<label><input type="checkbox" name="presentations" value="menu">Menu Plannning in LTC</label>
 			<label><input type="checkbox" name="presentations" value="ethics">Ethical Nutrition Considerations</label>
+		</div><br><br>
+		Each $10.00 <input type="number" name="quantity" min="0">
+		3 for $25.00 <input type="number" name="quantity" min="0">&nbsp;&nbsp;&nbsp;&nbsp;
+		22 (full CD) for $100 <input type="number" name="quantity" min="0">
+		<br><br>
+		<label class="product">4. Policy Pointers I, 4th Edition</label>
+		<p>Policies and Procedures for Nutrition and Hydration in Long Term Care</p>
+		CD <input type="number" name="quantity" min="0"> x $85.00
+		Manual <input type="number" name="quantity" min="0"> x $150.00<br>
+		Manual plus CD <input type="number" name="quantity" min="0"> x $195.00
+		<br><br>
+		<label class="product">5. Policy Pointers II, 4th Edition</label>
+		<p>Policies and Procedures for Dietary Services Administration in Long Term Care</p>
+		CD <input type="number" name="quantity" min="0"> x $85.00
+		Manual <input type="number" name="quantity" min="0"> x $135.00<br>
+		Manual plus CD <input type="number" name="quantity" min="0"> x $180.00
+		<br><br>
+		<label class="product">6. Algorithms, Protocols &amp; Tools</label>
+		<div class="small-12 medium-6 columns">
+			<label><input type="checkbox" name="presentations" value="hypoMant">Man't of Hypoglycemia</label>
+			<label><input type="checkbox" name="presentations" value="hydrationMant">Ass't &amp; Man't of Hydration</label>
+			<label><input type="checkbox" name="presentations" value="nutritionDelivery">Delivery of Nutrition Care</label>
+		</div>
+		<div class="small-12 medium-6 columns">
+			<label><input type="checkbox" name="presentations" value="skinMant">Man't of Impared Skin Integrity</label>
+			<label><input type="checkbox" name="presentations" value="enteralFeeding">Enteral Feeding Protocol</label>
+			Each $7.50 <input type="number" name="quantity" min="0">
 		</div>
 
 	</form>
