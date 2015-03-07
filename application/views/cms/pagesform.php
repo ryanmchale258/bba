fd177816d5c1724e9f4b7bd444a20ab8c57826ea
ryanmchale258 <r_mchale2@tss-hr312-dm29.local>
2015-03-07 17:15:39 -0500

    Validation
    and fuck

--- application/views/cms/pagesform.php
+++ application/views/cms/pagesform.php
@@ -4,11 +4,12 @@
 
 	<?php echo $formstart; ?>
 	<div class="row">
-		<label>Page Name</label>
+		<label>Page Name *</label>
 		<div class="small-12 medium-12 large-6 large-push-6 columns">
 			<p class="cmslabel">Will appear at the top of each page. It should be short/succinct.</p>
 		</div>
 		<div class="small-12 medium-12 large-6 large-pull-6 columns">
+			<span class="formerror"><?php echo form_error('pagename'); ?></span>
 			<?php echo $pagename; ?>
 		</div>
 	</div>
@@ -22,11 +23,12 @@
 		</div>
 	</div>
 	<div class="row">
-		<label>Meta Description. <span id="wordcount">150</span> characters remaining.</label>
+		<label>Meta Description. <span id="wordcount">150</span> characters remaining. *</label>
 		<div class="small-12 medium-12 large-6 large-push-6 columns">
 			<p class="cmslabel">The meta description appear beneath the page title in search engine results. It should describe each page accurately with a strong focus on relevant keywords. Max length is approximately 150 characters.</p>
 		</div>
 		<div class="small-12 medium-12 large-6 large-pull-6 columns">
+			<span class="formerror"><?php echo form_error('metadesc'); ?></span>
 			<?php echo $metadesc; ?>
 		</div>
 	</div>
@@ -37,36 +39,24 @@
 			<p class="cmslabel">If a parent item is chosen, the new page title will appear in a dropdown menu when hovering over the parent menu item. If the value is left as Orphan Page, it will not be accessible in any menu item and should be linked to from page text somewhere.</p>
 		</div>
 		<div class="small-6 medium-6 large-3 large-pull-6 columns">
-			<select name="parent">
-				<option selected>Orphan Page</option>
-				<?php foreach($navparents as $row): ?>
-					<option value="<?php echo $row->pages_slug; ?>"><?php echo $row->pages_title; ?></option>
-				<?php endforeach; ?>
-			</select>
+			<?php echo $parent; ?>
 		</div>
 		<div class="small-3 medium-3 large-1 large-pull-6 columns">
 			<label>Weight:</label>
 		</div>
 		<div class="small-3 medium-3 large-2 small-pull-1 medium-pull-2 large-pull-6 columns">
-			<select name="weight">
-				<option selected value="0">0</option>
-				<option value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-				<option value="4">4</option>
-				<option value="5">5</option>
-				<option value="6">6</option>
-			</select>
+			<?php echo $weight; ?>
 		</div>
 	</div>
 	
 	<div class="row">
-		<label>Page Content</label>
-		<div class="small-12 columns">
+		<label>Page Content *</label>
+		<div class="textareacont small-12 columns">
+			<span class="formerror"><?php echo form_error('content'); ?></span>
 			<?php echo $body; ?>
 		</div>
+		<input type="submit" class="formsubmit" name="submit" value="Submit">
 	</div>
 
-	<input type="submit" name="submit" value="Submit">
 	<?php if(isset($id)){ echo $id; } ?>
 </form>
