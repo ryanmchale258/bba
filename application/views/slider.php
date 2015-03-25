e0bc4dc17b5dd255a0b57237eb137404d9a8aa14
ChristopherRobins <chrisprobins87@gmail.com>
2015-03-24 21:15:18 -0400

    slider images

--- application/views/slider.php
+++ application/views/slider.php
@@ -1,8 +1,8 @@
 <div id="sliderwrapper" class="row">
 	<div id="homeSlider" class="fade small-12 columns">
-		<div><h2>Residential Clinical Care</h2><img src="<?php echo base_url(); ?>img/slide1.gif"></div>
-		<div><h2>Administrative Support</h2><img src="<?php echo base_url(); ?>img/slide2.gif"></div>
-		<div><h2>Quality Management</h2><img src="<?php echo base_url(); ?>img/slide3.gif"></div>
-		<div><h2>Resources/Training</h2><img src="<?php echo base_url(); ?>img/slide2.gif"></div>
+		<div><h2>Residential Clinical Care</h2><img src="<?php echo base_url(); ?>img/slider_residentialcare.jpg"></div>
+		<div><h2>Administrative Support</h2><img src="<?php echo base_url(); ?>img/slider_administrativesupport.jpg"></div>
+		<div><h2>Quality Management</h2><img src="<?php echo base_url(); ?>img/slider_qualitymanagement.jpg"></div>
+		<div><h2>Resources/Training</h2><img src="<?php echo base_url(); ?>img/slider_resourcestraining.jpg"></div>
 	</div>
 </div>
