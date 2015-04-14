fe70e13221cadf99909707878efbde8c062c1ea2
ryanmchale258 <ryan.mchale258@gmail.com>
2015-04-14 14:13:23 -0400

    Sidebar

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/barker/bba/';
+//$config['base_url']	= 'http://localhost/barker/bba/';
+$config['base_url']	= 'http://www.ryanmchale.ca/bba/';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +27,7 @@ $config['base_url']	= 'http://localhost/barker/bba/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php/';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
